86e1cec529303e8c8630294789ef3a8970582e3e
comt-mix <comt-mix@gmail.com>
2022-10-14 05:12:59 +0900

    ✨ Feat: ESLint 및 Prettier 세팅

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2022: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+  ],
+  rules: {
+    semi: "warn",
+    "no-unused-vars": "warn",
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: "true",
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+};
