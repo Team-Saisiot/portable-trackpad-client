ca49f14d25eb3bd23c5d83474ab3b793a8f7d09d
byeongseok <hallabong0126@gmail.com>
2022-10-14 23:04:40 +0900

    ✨ Feat: 클라이언트 socket 통신 테스트

--- App.js
+++ App.js
@@ -1,10 +1,33 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View, StyleSheet, TextInput } from "react-native";
+import { useEffect, useState } from "react";
+import { io } from "socket.io-client";
+import { SERVER_PORT } from "@env";
 
 export default function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    const socket = io(`${SERVER_PORT}`);
+    socket.emit("user-send", message);
+    console.log("useEffect ~ message", message);
+    socket.on("broadcast", (data) => {
+      console.log("socket.on ~ data", data);
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, [message]);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>{message}</Text>
+      <TextInput
+        style={styles.textInput}
+        onChangeText={(e) => setMessage(e)}
+      ></TextInput>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +39,12 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  textInput: {
+    margin: 15,
+    padding: 5,
+    borderStyle: "solid",
+    borderWidth: 1,
+    borderRadius: 5,
   },
 });
